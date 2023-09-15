b01dbab6c5598d9295b59cc89ce188e37ed6ac21
“Emeka <ngwe7236@gmail.com>
2023-09-16 00:03:57 +0200

    now now

--- src/components/IsAdminRoute.js
+++ src/components/IsAdminRoute.js
@@ -1,45 +1,45 @@
-import { useContext, useEffect } from "react";
-import { AuthContext } from "../context/auth.context";
-import { useNavigate } from "react-router-dom";
+// import { useContext, useEffect } from "react";
+// import { AuthContext } from "../context/auth.context";
+// import { useNavigate } from "react-router-dom";
 
-function IsAdminRoute({ children }) {
-  const { user } = useContext(AuthContext);
+// function IsAdminRoute({ children }) {
+//   const { user } = useContext(AuthContext);
  
 
-  const { isLoggedIn, isLoading } = useContext(AuthContext);
- const navigate = useNavigate()
-  // If the authentication is still loading 
-  if (isLoading) return <p>Loading ...</p>;
+//   const { isLoggedIn, isLoading } = useContext(AuthContext);
+//  const navigate = useNavigate()
+//   // If the authentication is still loading 
+//   if (isLoading) return <p>Loading ...</p>;
  
  
 
-  if (!isLoggedIn) {
-  // If the user is not logged in 
-     navigate("/login")
-  } else {
-  // If the user is logged in, allow to see the page 
-    return children;
-  }
-}
+//   if (!isLoggedIn) {
+//   // If the user is not logged in 
+//      navigate("/login")
+//   } else {
+//   // If the user is logged in, allow to see the page 
+//     return children;
+//   }
+// }
 
 
 
-  useEffect(() => {
-    // Log the user object for debugging purposes
-    console.log("User Object:", user);
+//   useEffect(() => {
+//     // Log the user object for debugging purposes
+//     console.log("User Object:", user);
 
-    // Check if the user is an admin
-    if (user && user.role === "admin") {
-      console.log("User is an admin. Allow access.");
-    } else {
+//     // Check if the user is an admin
+//     if (user && user.role === "admin") {
+//       console.log("User is an admin. Allow access.");
+//     } else {
 
-      console.log("User is not an admin. Redirecting to home.");
-      navigate("/");
-    }
-  }, [user, navigate]);
+//       console.log("User is not an admin. Redirecting to home.");
+//       navigate("/");
+//     }
+//   }, [user, navigate]);
 
-  // Render children only if user is an admin
-  return user && user.role === "admin" ? children : null;
-}
+//   // Render children only if user is an admin
+//   return user && user.role === "admin" ? children : null;
+// }
 
-export default IsAdminRoute;
+// export default IsAdminRoute;
